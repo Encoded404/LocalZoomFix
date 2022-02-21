27b0df10b988b912c0ab1ba1027f9b1105106ec7
pdcook <qphysicscook@gmail.com>
2022-02-21 13:21:15 -0500

    forgot to git add files for PR #1

---
+++ Extensions/PlayerActions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using InControl;
+
+namespace LocalZoom.Extensions
+{
+    // this extension stores ONLY the data for additional player actions
+    // additional actions are assigned in the PlayerActions patches
+    public class PlayerActionsAdditionalData
+    {
+        public PlayerAction zoomIn;
+        public PlayerAction zoomOut;
+    }
+    public static class PlayerActionsExtension
+    {
+        public static readonly ConditionalWeakTable<PlayerActions, PlayerActionsAdditionalData> data =
+            new ConditionalWeakTable<PlayerActions, PlayerActionsAdditionalData>();
+
+        public static PlayerActionsAdditionalData GetAdditionalData(this PlayerActions playerActions)
+        {
+            return data.GetOrCreateValue(playerActions);
+        }
+
+        public static void AddData(this PlayerActions playerActions, PlayerActionsAdditionalData value)
+        {
+            try
+            {
+                data.Add(playerActions, value);
+            }
+            catch (Exception) { }
+        }
+
+        public static int PlayerZoom(this PlayerActions playerActions)
+        {
+            if (playerActions.GetAdditionalData().zoomIn.IsPressed) { return -1; }
+            else if (playerActions.GetAdditionalData().zoomOut.IsPressed) { return +1; }
+            else { return 0; }
+        }
+    }
+}
---
+++ Patches/PlayerActions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HarmonyLib;
+using LocalZoom.Extensions;
+using System.Reflection;
+using InControl;
+
+namespace LocalZoom.Patches
+{
+    // postfix PlayerActions constructor to add controls
+    [HarmonyPatch(typeof(PlayerActions))]
+    [HarmonyPatch(MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { })]
+    class PlayerActionsPatchPlayerActions
+    {
+        private static void Postfix(PlayerActions __instance)
+        {
+            __instance.GetAdditionalData().zoomIn = (PlayerAction)typeof(PlayerActions).InvokeMember("CreatePlayerAction",
+                                    BindingFlags.Instance | BindingFlags.InvokeMethod |
+                                    BindingFlags.NonPublic, null, __instance, new object[] { "Zoom In" });
+            __instance.GetAdditionalData().zoomOut = (PlayerAction)typeof(PlayerActions).InvokeMember("CreatePlayerAction",
+                                    BindingFlags.Instance | BindingFlags.InvokeMethod |
+                                    BindingFlags.NonPublic, null, __instance, new object[] { "Zoom Out" });
+
+        }
+    }
+    // postfix PlayerActions to add controller controls
+    [HarmonyPatch(typeof(PlayerActions), "CreateWithControllerBindings")]
+    class PlayerActionsPatchCreateWithControllerBindings
+    {
+        private static void Postfix(ref PlayerActions __result)
+        {
+            __result.GetAdditionalData().zoomIn.AddDefaultBinding(InputControlType.DPadUp);
+
+            __result.GetAdditionalData().zoomOut.AddDefaultBinding(InputControlType.DPadDown);
+        }
+    }
+    // postfix PlayerActions to add keyboard controls
+    [HarmonyPatch(typeof(PlayerActions), "CreateWithKeyboardBindings")]
+    class PlayerActionsPatchCreateWithKeyboardBindings
+    {
+        private static void Postfix(ref PlayerActions __result)
+        {
+            __result.GetAdditionalData().zoomIn.AddDefaultBinding(Mouse.PositiveScrollWheel);
+
+            __result.GetAdditionalData().zoomOut.AddDefaultBinding(Mouse.NegativeScrollWheel);
+        }
+    }
+}
