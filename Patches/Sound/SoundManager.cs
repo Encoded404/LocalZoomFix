93f6c73b67fbc1fba9eec642c22291fb40d64dd5
pdcook <qphysicscook@gmail.com>
2022-03-10 18:24:13 -0500

    added patches for overloads of SoundManager.Play for directional sound

--- Patches/Sound/SoundManager.cs
+++ Patches/Sound/SoundManager.cs
@@ -30,4 +30,74 @@ namespace LocalZoom.Patches
             return false;
         }
     }
+    [HarmonyPatch(typeof(SoundManager), "Play")]
+    [HarmonyPatch(new Type[] { typeof(SoundEvent), typeof(Transform), typeof(SoundTag) })]
+    public class SoundManagerPlayPatch2
+    {
+        public static bool Prefix(SoundManager __instance, SoundEvent soundEvent, Transform owner, SoundTag localSoundTag)
+        {
+            if (LocalZoom.IsInOfflineModeAndNotSandbox || !LocalZoom.enableShaderSetting || owner == null || soundEvent == null)
+                return true;
+            if (owner == SoundManager.Instance.GetTransform())
+            {
+                return true;
+            }
+
+            if (!(__instance.GetFieldValue("data") is SoundManagerData data))
+            {
+                UnityEngine.Debug.LogError("SoundManagerData is null");
+                return true;
+            }
+            data.PlaySoundEvent(soundEvent, PlayType.Play, owner, null, owner, null, null, null, null, localSoundTag);
+
+            return false;
+        }
+    }
+    [HarmonyPatch(typeof(SoundManager), "Play")]
+    [HarmonyPatch(new Type[] { typeof(SoundEvent), typeof(Transform), typeof(SoundParameterBase[]) })]
+    public class SoundManagerPlayPatch3
+    {
+        public static bool Prefix(SoundManager __instance, SoundEvent soundEvent, Transform owner, SoundParameterBase[] soundParameterBaseArray)
+        {
+            if (LocalZoom.IsInOfflineModeAndNotSandbox || !LocalZoom.enableShaderSetting || owner == null || soundEvent == null)
+                return true;
+            if (owner == SoundManager.Instance.GetTransform())
+            {
+                return true;
+            }
+
+            if (!(__instance.GetFieldValue("data") is SoundManagerData data))
+            {
+                UnityEngine.Debug.LogError("SoundManagerData is null");
+                return true;
+            }
+            data.PlaySoundEvent(soundEvent, PlayType.Play, owner, null, owner, null, null, soundParameterBaseArray, null, null);
+
+            return false;
+        }
+    }
+    [HarmonyPatch(typeof(SoundManager), "Play")]
+    [HarmonyPatch(new Type[] { typeof(SoundEvent), typeof(Transform), typeof(SoundTag), typeof(SoundParameterBase[]) })]
+    public class SoundManagerPlayPatch4
+    {
+        public static bool Prefix(SoundManager __instance, SoundEvent soundEvent, Transform owner, SoundTag localSoundTag, SoundParameterBase[] soundParameterBaseArray)
+        {
+            if (LocalZoom.IsInOfflineModeAndNotSandbox || !LocalZoom.enableShaderSetting || owner == null || soundEvent == null)
+                return true;
+            if (owner == SoundManager.Instance.GetTransform())
+            {
+                return true;
+            }
+
+            if (!(__instance.GetFieldValue("data") is SoundManagerData data))
+            {
+                UnityEngine.Debug.LogError("SoundManagerData is null");
+                return true;
+            }
+            data.PlaySoundEvent(soundEvent, PlayType.Play, owner, null, owner, null, null, soundParameterBaseArray, null, localSoundTag);
+
+            return false;
+        }
+    }
+
 }
